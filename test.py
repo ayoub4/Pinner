6b474880915236fd6cc9dd02e9e164d0e9df5953
Setup Game <ayoubmzari33@gmail.com>
2023-05-26 15:25:31 +0100

    fdqds

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 # Define the API endpoint URL
-api_url = 'http://127.0.0.1:8000/pin'
+api_url = ' http://164.68.105.49:8000/pin'
 
 # Define the pin URL to send in the request
 pin_url = 'https://couplesamoureux.com/wp-content/uploads/2023/03/alliances-coeur-graves-700206_1200x.webp'
