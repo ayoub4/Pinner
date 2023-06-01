a436b3349596090ca0e022907bd784d55a0a96ca
Setup Game <ayoubmzari33@gmail.com>
2023-06-01 20:09:26 +0100

    fdqds

--- app.py
+++ app.py
@@ -31,10 +31,7 @@ class PinPoster:
     def __init__(self, account):
         self.account = account
         self.chrome_options = webdriver.ChromeOptions()
-        self.chrome_options.add_argument('--headless')
-        self.chrome_options.add_argument('--window-size=1920x1080')
-        self.chrome_options.add_argument('--no-sandbox')
-        self.chrome_options.add_argument('--disable-dev-shm-usage')
+
         self.driver = None
 
     def find_closest_image(self, img_url, images_list):
@@ -71,7 +68,7 @@ class PinPoster:
 
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '[data-test-id="landing-page"]')))
 
-        create_pin_button = self.driver.find_element(By.XPATH, '//*[@id="HeaderContent"]/div/div/div/div/div[2]/div/div/div/div[2]/div/button/div/div')
+        create_pin_button = self.driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div[1]/div[2]/div/div/div/div/div[2]/div/div/div/div[2]/div/button/div')
         create_pin_button.click()
 
         WebDriverWait(self.driver, 10).until(
