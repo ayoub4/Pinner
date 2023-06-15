ffcf4e757443ed67947bc2f79e780fb3554cb69d
Setup Game <ayoubmzari33@gmail.com>
2023-06-15 14:50:26 +0100

    fdqds

--- app.py
+++ app.py
@@ -32,10 +32,10 @@ class PinPoster:
     def __init__(self, account):
         self.account = account
         self.chrome_options = webdriver.ChromeOptions()
-        """self.chrome_options.add_argument('--headless')
+        self.chrome_options.add_argument('--headless')
         self.chrome_options.add_argument('--window-size=1920x1080')
         self.chrome_options.add_argument('--no-sandbox')
-        self.chrome_options.add_argument('--disable-dev-shm-usage')"""
+        self.chrome_options.add_argument('--disable-dev-shm-usage')
         self.driver = None
 
     def find_closest_image(self, img_url, images_list):
@@ -524,6 +524,127 @@ class PinPoster:
 
 
     def create_pin_robe_princesse(self, pin_details):
+        try:
+            self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=self.chrome_options)
+            self.driver.get('https://www.pinterest.fr/login/')
+
+            email_input = WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((By.XPATH, '//*[@id="email"]')))
+            email_input.send_keys(self.account.email)
+
+            password_input = self.driver.find_element(By.XPATH, '//*[@id="password"]')
+            password_input.send_keys(self.account.password)
+
+            login_button = self.driver.find_element(By.XPATH,
+                                                    '//*[@id="mweb-unauth-container"]/div/div[3]/div/div/div[3]/form/div[7]/button')
+            login_button.click()
+
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '[data-test-id="landing-page"]')))
+
+            create_pin_button = self.driver.find_element(By.XPATH,
+                                                         '/html/body/div[1]/div/div[1]/div/div[1]/div[2]/div/div/div/div/div[2]/div/div/div/div[2]/div/button/div')
+            create_pin_button.click()
+
+            WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located(
+                    (By.XPATH, '//*[@id="create-menu-content"]'))
+            )
+            create_menu_content = self.driver.find_element(By.XPATH, '//*[@id="create-menu-content"]')
+            create_pin_button_2 = create_menu_content.find_element(By.XPATH, './/*[text()="Créer une Épingle Idée"]')
+            create_pin_button_2.click()
+
+            """WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located(
+                    (By.XPATH, '//*[@id="__PWS_ROOT__"]/div/div[1]/div/div[2]/div/div/div/div[3]/div'))
+            )
+            create_pin_button_3 = WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((By.XPATH, './/*[text()="Créer une nouvelle Épingle"]'))
+            )
+            create_pin_button_3.click()"""
+
+            create_pin_by_url = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, '//*[text()="Créer des Épingles à partir d’une URL"]')))
+            create_pin_by_url.click()
+
+            pin_url_input = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.ID, 'scrape-view-website-link')))
+            pin_url_input.send_keys(pin_details.pin_url)
+
+            pin_url_go = self.driver.find_element(By.XPATH,
+                                                  '//*[@id="__PWS_ROOT__"]/div/div[1]/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div/div/div/div/div[1]/div/div[3]/div/div/button/div/div')
+            pin_url_go.click()
+
+            time.sleep(3)
+            image_elements = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_all_elements_located((By.XPATH, '//img[contains(@alt, "Sélectionner l\'image dans")]'))
+            )
+            # Get the src attribute values
+            image_sources = [image_element.get_attribute('src') for image_element in image_elements]
+            result = self.find_closest_image(pin_details.img_url, image_sources)
+
+            for image_element in image_elements:
+                if image_element.get_attribute('src') == result:
+                    image_element.click()
+                    break
+
+            pin_confirm = self.driver.find_element(By.XPATH,
+                                                   '//*[@id="__PWS_ROOT__"]/div/div[1]/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div[2]/button/div/div')
+            pin_confirm.click()
+
+            # Wait for the pin title input to be present
+            pin_title_input = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH,
+                                                '/html/body/div[1]/div/div[1]/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div/div[1]/div[1]/div/div/div[1]/textarea'))
+            )
+            pin_title_input.send_keys(pin_details.pin_title)
+
+            # Wait for the pin description input to be present
+            pin_description_input = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH,
+                                                '/html/body/div[1]/div/div[1]/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div/div[1]/div[3]/div/div[1]/div/div/div[1]/div/div[2]/div/div/div/div'))
+            )
+            pin_description_input.send_keys(pin_details.pin_description)
+
+            time.sleep(0.5)
+
+            tables_button = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '[data-test-id="board-dropdown-select-button"]'))
+            )
+            tables_button.click()
+
+            tables_div = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH,
+                                                '/html/body/div[1]/div/div[1]/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div/div/div/div/div[1]/div/div[2]/div/div/div[2]/div/div/div/div/div/div/div/div/div[2]'))
+            )
+            desired_element = tables_div.find_element(By.XPATH, f'.//*[contains(text(), "{pin_details.pin_table}")]')
+            desired_element.click()
+
+            time.sleep(0.5)
+
+            submit_button = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '[data-test-id="board-dropdown-save-button"]'))
+            )
+            publish_button = submit_button.find_element(By.XPATH, './/*[contains(text(), "Publier")]')
+            time.sleep(0.5)
+            publish_button.click()
+
+            # Wait for the pin to be created
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, 'button[aria-label="dismiss"]'))
+            )
+
+            # Close the browser
+            self.driver.quit()
+
+            return "Pin posted: " + pin_details.pin_title
+        except Exception as e:
+            print("An error occurred:", e)
+        finally:
+            if self.driver:
+                self.driver.quit()
+
+    def create_pin_pyjama_dor(self, pin_details):
         try:
             self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=self.chrome_options)
             self.driver.get('https://www.pinterest.fr/login/')
@@ -1181,6 +1302,9 @@ def pin():
         elif website == 'applique murale':
             email = 'mamouncherkaoui12345@gmail.com'
             password = 'Szzk@2za'
+        elif website == 'pyjama d\'or':
+            email = 'iamfiles123@gmail.com'
+            password = 'bhjbhj2@Ss'
         else:
             return "Error: Not The Right Website"
 
@@ -1247,7 +1371,12 @@ def process_pin_queue():
                 elif pin_poster.account.email == "mamouncherkaoui12345@gmail.com":
                     print("Posting pin : " + pin_details.pin_title + "...")
                     response = pin_poster.create_pin_applique_murale(pin_details)
-                    print(response)  # Print the response
+                    print(response)
+                elif pin_poster.account.email == "iamfiles123@gmail.com":
+                    print("Posting pin : " + pin_details.pin_title + "...")
+                    response = pin_poster.create_pin_pyjama_dor(pin_details)
+                    print(response)
+                # Print the response
 
             except Exception as e:
                 print("An error occurred while posting pin:", str(e))
--- test.py
+++ test.py
@@ -7,7 +7,7 @@ api_url = 'http://127.0.0.1:8000/pin'
 pin_url = 'https://montapispriere.com/tapis-de-priere-uni/tapis-de-priere-beige/tapis-de-priere-beige-argente/'
 url='https://universcendrier.com/pin?pin_url=https%253A%252F%252Fcouplesamoureux.com%252Fwp-content%252Fuploads%252F2023%252F03%252Falliances-coeur-graves-700206_1200x.webp'
 # Send the POST request to the API
-response = requests.get("http://127.0.0.1:8000/pin?pin_url=https://marobeboheme.com/robe-longue-boheme/robe-cocktail-style-boheme-chic-florale-rose-longue/&pin_description=&pin_title=Robe%20cocktail%20style%20boh%C3%A8me%20chic%20Florale%20rose%20longue&img_url=https://marobeboheme.com/wp-content/uploads/2023/06/Robe-Longue-Manches-sulfpour-Femme-Mode-Cor-enne-l-gante-Florale-D-contract-e-Vacances-Printemps.jpg_640x640-150x150.jpg&website=ma%20robe%20boheme&pin_table=Robe%20Longue%20Boh%C3%A8me")
+response = requests.get("http://127.0.0.1:8000/pin?pin_url=https://pyjamador.com/kigurumi/kigurumi-animaux/kigurumi-pingouin/kugurumi-pingouin-pour-adulte-rose/&pin_description=&pin_title=Kugurumi%20pingouin%20pour%20adulte%20rose&img_url=https://pyjamador.com/wp-content/uploads/2023/04/S17956326fbd642cea87d5d72e5e5ae92r-150x150.jpg&website=pyjama%20d%27or&pin_table=Kigurumi")
 
 # Check the response
 if response.status_code == 200:
